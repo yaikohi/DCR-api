70b28d298c10c019db04da3f8f1dd107ed213278
Erik Beem <erik.beem@hotmail.com>
2021-04-01 09:02:40 +0200

    dcr works now

--- api/main.py
+++ api/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-# from pydantic import BaseModel
-# from pydantic.types import Json
 import json
 import requests 
 
@@ -27,33 +25,22 @@ app = FastAPI(
     openapi_tags=tags_metadata
 )
 
-# Loading the databases
-# Logos
+# Loading the company logo database from local storage
 db = "../data/db_logos.json"
 db = json.load(open(db))
 
-# Full
-api_url = "https://dashboard-pio.herokuapp.com/companies"
-request = requests.get(api_url).json()
-db_api = request['response']
 
-# !: Probably not very useful.
-# # Returns the complete database
-# @app.get("/", tags=["Database"])
-# async def get_database():
-#     return db_api
-
-# Returns all the logo's.
-@app.get("/logos", tags=["Logos"])
-def get_logos():
+# Returns company logos
+@app.get("/", tags=["Logos"])
+async def get_logos():
     return db
 
 # Returns the logo dict of a specific company specified in the url.
-@app.get("/logos/{company_name}", tags=["Logos"])
-async def get_colors(company_name: str) -> dict:
+@app.get("/{company_name}", tags=["Logos"])
+async def get_logo_colors(company_name: str) -> dict:
     return db[company_name]['logo']['colors']
 
-
+# TODO: For updating logo colours of newly added companies. (?)
 # @app.patch("/logos/colors/{company_name}", tags=["Update Logo colours"])
 # async def update_logo_colors(company_name: str, colors: Colors):
 #     company_logo_url = db[company_name]['logo']['url']
