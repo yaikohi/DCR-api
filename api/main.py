9d8bcd72d18aa7eb5031fb5e9f3646a6f524c032
Erik Beem <erik.beem@hotmail.com>
2021-04-07 13:10:42 +0200

    Added api metadata.

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests 
 
 # ? Pylance underlines this import with a warning but it compiles fine. ( "Import 'dcr' could not be resolved - PylancereportMissingImports" )
@@ -9,11 +9,16 @@ from dcr import get_dominant_color
 tags_metadata = [
     {
         "name": "Logos",
-        "description": "The logos of the companies in the database.",
+        "description": "The logos of the companies.",
+    },
+    {
+        "name": "Companies",
+        "description": "All the information of the companies."
     }
 ]
 
 
+# TODO: Write schemas for in- and output responses.
 app = FastAPI(
     title="DCR-api",
     description="For pioDash CMS.",
@@ -22,70 +27,83 @@ app = FastAPI(
 )
 
 
-# ! LOADING THE NECESSARY DATA 
-# Using a temporary memory-based database bypasses the need to create my own database.
+# ! LOADING DATA FROM THE DATABASE
+# Fetches the data from the database and constructs a local memory cache for queries.
 # TODO: Double check the variable names for readability.
 # TODO: Possible restrucuring of code.
-
-data = requests.get("https://dashboard-pio.herokuapp.com/companies").json()['response']
+db = requests.get("https://dashboard-pio.herokuapp.com/companies").json()['response']
 url_first_half = "https://dashboard-pio.herokuapp.com"
 
-list_of_logo_urls = []
+# Lists to be filled with logo-urls and company-names.
 list_of_companies = []
-for i in range(len(data)):
+list_of_logo_urls = []
+
+
+# looping over the dashboard-pio database
+for i in range(len(db)):
     
-    # list of companies
-    company_name = data[i]['name']
+    # Filling the list_of_company_names with company_names
+    company_name = db[i]['name']
     if company_name in list_of_companies:
         pass
     else:
         list_of_companies.append(company_name)
     
-    # list of logo urls
-    url = data[i]['logo']
+    # Filling the list_of_logo_urls with logo_urls
+    url = db[i]['logo']
     if url in list_of_logo_urls:
         pass
     else: 
         list_of_logo_urls.append(url_first_half+url)
-    
-logos_zip = zip(list_of_companies, list_of_logo_urls)
-logos_dict = dict(logos_zip)
-    
+
+# Dict that stores all the logo-urls of every company. (company_name = key, logo_url = value)
+logos_dict = dict(zip(list_of_companies, list_of_logo_urls))
 
 
 
-# ! API ROUTES BELOW
-# Returns company logos
-@app.get("/", tags=["Logos"])
+# ! API ROUTES
+# Returns the full database.
+@app.get("/", tags=["Companies"])
 async def get_logos():
-    return data
-
-
-# Returns the logo dict of a specific company specified in the url.
-@app.get("/{company_name}", tags=["Logos"])
-async def get_logo_colors(company_name: str) -> dict:
-    for i in range(len(data)):
-        if company_name == data[i]['name']:
-            return data[i]
+    return db
 
 
 # TODO: Add a spelling check algorithm for the company names so that it corrects typos to the corresponding company name. This will make it more user-friendly.
+# Returns the logo dict of a company specified in the url.
+@app.get("/{company_name}", tags=["Logos"])
+async def get_logo_colors(company_name: str) -> dict:
+
+    # Raises an error when a typo occurs or the name is not in the database.    
+    if company_name not in list_of_companies:
+        raise HTTPException(status_code=404, 
+        detail="404: Company not found. Mind case-sensitivity and use of spaces.")
+    for i in range(len(db)):
+        if company_name == db[i]['name']:
+            return db[i]
+
+
+# TODO: Add a way of checking the code runtime. Return the time data in the get request?
+# TODO: Add a spelling check algorithm for the company names so that it corrects typos to the corresponding company name. This will make it more user-friendly.
 # Returns the list of colors when the request body contains an url to the company logo.
 @app.get("/{company_name}/colors", tags=["Logos"])
-async def return_logo_colors(company_name: str) -> list:    
-    if company_name in list_of_companies:
-        url = logos_dict[company_name]
-        
-        return get_dominant_color(url)
+async def return_logo_colors(company_name: str) -> list:
+
+    # Raises an error when a typo occurs or the name is not in the database.    
+    if company_name not in list_of_companies:
+        raise HTTPException(status_code=404, 
+        detail="404: Company not found. Mind case-sensitivity and use of spaces.")
+
+    url = logos_dict[company_name]
+    return get_dominant_color(url)
 
 
-# Old route, legacy code 
+# -- legacy code -- 
 # // # Returns the list of colors when the request body contains an url to the company logo.
 # //@app.get("/{company_name}/colors", tags=["Logos"])
 # // async def return_logo_colors(company_name: str) -> list:    
-# //    for i in range(len(data)):
-# //        if company_name == data[i]['name']:
-# //            url_second_half = data[i]['logo']
+# //    for i in range(len(db)):
+# //        if company_name == db[i]['name']:
+# //            url_second_half = db[i]['logo']
 # //            url = str(url_first_half + url_second_half)
 # //            return get_dominant_color(url)
 # //        else: 
