3c72340ff4ed43c017de1e3599cd1540551d19b6
Erik Beem <erik.beem@hotmail.com>
2021-04-16 12:29:47 +0200

    idk

--- api/main.py
+++ api/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, HTTPException
 import requests 
-
-# ? Pylance underlines this import with a warning but it compiles fine. ( "Import 'dcr' could not be resolved - PylancereportMissingImports" )
 from dcr import get_dominant_color
 
 
-# Description for the docs page. ( http://127.0.0.1:8000/docs )
+# Description for the docs page (visible at http://127.0.0.1:8000/docs ).
 tags_metadata = [
     {
         "name": "Logos",
@@ -26,39 +24,13 @@ app = FastAPI(
     openapi_tags=tags_metadata
 )
 
-
 # Fetches the data from the database and constructs a local memory cache for queries.
 # TODO: Double check the variable names for readability.
-# TODO: Possible restrucuring of code.
+# TODO: Add a HTTP error handler for the GETrequest.
 db = requests.get("https://dashboard-pio.herokuapp.com/companies").json()['response']
-url_first_half = "https://dashboard-pio.herokuapp.com"
 
-# Lists to be filled with logo-urls and company-names.
-list_of_companies = []
-list_of_logo_urls = []
-
-
-# looping over the dashboard-pio database
-for i in range(len(db)):
-    
-    # Filling the list_of_company_names with company_names
-    company_name = db[i]['name']
-    if company_name in list_of_companies:
-        pass
-    else:
-        list_of_companies.append(company_name)
-    
-    # Filling the list_of_logo_urls with logo_urls
-    url = db[i]['logo']
-    if url in list_of_logo_urls:
-        pass
-    else: 
-        list_of_logo_urls.append(url_first_half+url)
-
-# Dict that stores all the logo-urls of every company. (company_name = key, logo_url = value)
-logos_dict = dict(zip(list_of_companies, list_of_logo_urls))
-
-
+# Dict that stores all the logo-urls of every company: {"company_name": "logo_url"}. // # Thanks Aswin :)
+company_logos_dict = {company['name']: f"https://dashboard-pio.herokuapp.com{company['logo']}" for company in db}
 
 # Returns the full database.
 @app.get("/", tags=["Companies"])
@@ -72,40 +44,26 @@ async def get_logos():
 async def get_logo_colors(company_name: str) -> dict:
 
     # Raises an error when a typo occurs or the name is not in the database.    
-    if company_name not in list_of_companies:
+    if company_name not in company_logos_dict.keys():
         raise HTTPException(status_code=404, 
         detail="404: Company not found. Mind case-sensitivity and use of spaces.")
+
     for i in range(len(db)):
         if company_name == db[i]['name']:
             return db[i]
 
 
 # ? Add an endpoint to see the clusters in a graph?
-# TODO: Add a way of checking the code runtime. Return the time data in the get request?
 # TODO: Add a spelling check algorithm for the company names so that it corrects typos to the corresponding company name. This will make it more user-friendly.
 # Returns the list of colors when the request body contains an url to the company logo.
 @app.get("/{company_name}/colors", tags=["Logos"])
 async def return_logo_colors(company_name: str) -> list:
 
+    # ? TODO: Create a error handler function in a seperate file.
     # Raises an error when a typo occurs or the name is not in the database.    
-    if company_name not in list_of_companies:
+    if company_name not in company_logos_dict.keys():
         raise HTTPException(status_code=404, 
         detail="404: Company not found. Mind case-sensitivity and use of spaces.")
 
-    url = logos_dict[company_name]
-    return get_dominant_color(url)
-
-
-# -- legacy code -- 
-# // # Returns the list of colors when the request body contains an url to the company logo.
-# //@app.get("/{company_name}/colors", tags=["Logos"])
-# // async def return_logo_colors(company_name: str) -> list:    
-# //    for i in range(len(db)):
-# //        if company_name == db[i]['name']:
-# //            url_second_half = db[i]['logo']
-# //            url = str(url_first_half + url_second_half)
-# //            return get_dominant_color(url)
-# //        else: 
-# //            # ! Not safe. 
-# //            # TODO: Add error handler.
-# //            continue
+    company_logo_url = company_logos_dict[company_name]
+    return get_dominant_color(company_logo_url)
--- test.py
+++ test.py
@@ -1,28 +1,54 @@
-import requests
+# ? Learning story
+# import requests
 
-url = "https://dashboard-pio.herokuapp.com/companies"
-response = requests.get(url).json()
+# url = "https://dashboard-pio.herokuapp.com/companies"
+# response = requests.get(url).json()
+# db = response['response']
+# url_first_half = "https://dashboard-pio.herokuapp.com"
 
-data = response['response']
+# # * Aswin's feedback:
+# logos_dict = {company['name']: f"https://dashboard-pio.herokuapp.com{company['logo']}" for company in db}
 
-print(type(data[0]['name']))    # str
-print(type(data))               # list
-
-
-
-
-for i in range(len(data)):
-    company_name = 'Fynch'
-    if data[i]['name'] == company_name:        
-        logo_url_half = "https://dashboard-pio.herokuapp.com"
-        logo_url_second_half = data[i]['logo']
-        
-        print(f'{logo_url_half + logo_url_second_half} it worked bro')
-    
-    
-# logo_url_half = "https://dashboard-pio.herokuapp.com"
-# logo_url_second_half = data[0]['logo']
-# # logo_url = str(logo_url_half + logo_url_second_half)
+# print(logos_dict)
 
 
-# print(logo_url)
+# ! Trials below
+# ? Pokemon API example; src: 
+# async def main():
+#     async with ClientSession() as session:
+#         for number in range(1, 15):
+#             pokemon_url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+#             async with session.get(pokemon_url) as respOnse:
+#                 pokemon = await respOnse.json()
+#                 print(pokemon['name'])
+
+# asyncio.run(main())
+
+
+# ? Learning story
+async def fetch_data():
+    print(f'start fetch')
+    await asyncio.sleep(0.01)
+
+    print(f'done fetching')
+
+async def print_nums():
+    print(f'start print nums')
+    for i in range(20):
+        print(f'num: {i}')
+        await asyncio.sleep(0.01)
+
+async def main():
+    start_time = time.time()
+
+
+    task1 = asyncio.create_task(fetch_data())
+    task2 = asyncio.create_task(print_nums())
+
+    value1 = await task1
+    print(value1)
+    await task2
+    print("It took %s seconds " % (time.time() - start_time))
+
+
+asyncio.run(main())
