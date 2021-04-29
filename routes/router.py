5eaf9547aef5215da413c4a761cf2acf9cf02afe
yaikohi <erik.beem@student.hu.nl>
2021-04-29 11:58:21 +0200

    Updated the endpoint routes.

--- routes/router.py
+++ routes/router.py
@@ -1,10 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
 import asyncio
+import markdown as md
 from services.dcr import get_dominant_colors
 from services.fetch_data import fetch_data
 
 router = APIRouter()
+# TODO: 1. Seperate the endpoints. There are too many in this file now.
 
+# * Optional todos:
+# TODO: 2. Learn jinja2. 
+# TODO: 3. Add a new endpoint for a readme.md file.
+# from fastapi.templating import Jinja2Templates
+# templates = Jinja2Templates(directory="docs")
+# # /API
+# @router.get("/", tags=["readme"], response_class=HTMLResponse)
+# async def show_readme(request: Request):
+#     with open("./docs/README.md", "r", encoding="utf-8") as input_file:
+#         text = input_file.read()
+#     html = md.markdown(text)
+#     data = {
+#         "title": "Readme",
+#         "text": html
+#     }
+#     return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 # IMAGES
 @router.get("/colors/{img_url}", tags=["colors"])
@@ -24,33 +44,66 @@ response = asyncio.get_event_loop().run_until_complete(fetch_data(url="https://d
 db = response['data']
 
 @router.get("/piodash-colors/{company_id}", tags=["piodash-colors"])
-async def get_colors_of_a_company(company_id: str) -> list:
-    colors_list = []
-    test = []
-    # Loops over all the companies in the database
+async def get_colors_of_a_company(company_id: str) -> list: 
+    """
+    Input: a company_id from the database.
+    Output: returns a list of hexadecimal RGB color values
+    """
     for i in range(len(db)):
-        # and checks for matches with the company_id
-    #     while company_id != db[i]['id']:
-    #         try:
-    #             return f'Loading...'
-    #         except company_id == db[i]['id']:
-    #             colors_list.append(db[i]['id'])
-    # return colors_list
+        if company_id in db[i]['id']:
+            url = url_base + db[i]['logo']
+            colors = get_dominant_colors(url)
+            return colors
+        else:
+            continue
 
+@router.get("/piodash-colors/", tags=["piodash-colors"])
+async def get_colors_of_a_company() -> dict: 
+    """
+    Returns a dict with key = company_name and,
+    value = [logo_colors] where logo_colors is a list 
+    containing three hexadecimal RGB color-values.
+    Output: 
+    """
+    piodash_color_dict = {}
+    
+    for i in range(len(db)):
+        company_name = db[i]['name']
+        
+        url = url_base + db[i]['logo']
+        company_colors = get_dominant_colors(url)
+        
+        piodash_color_dict[f'{company_name}'] = company_colors
+    return piodash_color_dict
 
 
 @router.get("/piodash/", tags=["piodash-colors"])
 async def get_piodash_db() -> dict:
+    """
+    Simply returns a database for now.
+
+    Returns:
+        dict: [the fetched-database]
+    """
     return db
 
 
 @router.get("/piodash/ids/", tags=["piodash-colors"])
 async def get_piodash_company_ids():
-    id_list = []
-    for i in range(len(db)):
-        id_list.append(db[i]['id'])
+    """"
+    Returns a dict with company names as keys 
+    and id's as values.
     
-    return id_list
+    Returns:
+        dict: [key: company_name, value: company_id]
+    """
+    piodash_id_dict = {}
+    
+    for i in range(len(db)):
+        company_id = db[i]['id']
+        company_name = db[i]['name']
+        piodash_id_dict[f'{company_name}'] = company_id
 
+    return piodash_id_dict
 
 asyncio.get_event_loop().close()
