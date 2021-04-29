d582212efb2da550c5f5598bae9296501805c02e
yaikohi <erik.beem@student.hu.nl>
2021-04-29 09:38:37 +0200

    Cleaned async req code

--- services/fetch_data.py
+++ services/fetch_data.py
@@ -1,4 +1,4 @@
-# # Asynchronous GETrequest to dashboard-pio.herokuapp
+# Asynchronous GETrequest to dashboard-pio.herokuapp/companies
 import aiohttp
 import asyncio
 
@@ -10,13 +10,9 @@ async def fetch_data(url: str):
     """
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-
-            # print("Status:", response.status)
-            # print("Content-type:", response.headers['content-type'])
-
             data = await response.json()
             return {"data": data['response'], "status": response.status, "headers": response.headers}
 
-# if __name__ == "__main__":
-#     data = asyncio.get_event_loop().run_until_complete(fetch_data(url="https://dashboard-pio.herokuapp.com/companies"))
-#     print(data["data"][1]["id"])
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(fetch_data(url="https://dashboard-pio.herokuapp.com/companies"))
