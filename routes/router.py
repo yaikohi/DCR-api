31162297647bab542dcac0ae432fd4c83e6cf279
Erik Beem <erik.beem@hotmail.com>
2021-04-30 14:00:27 +0200

    Moved the image endpoints to a single file.

--- routes/router.py
+++ routes/router.py
@@ -6,13 +6,15 @@ from routes import piodash
 router = APIRouter()
 
 router.include_router(piodash.router)
-
-# TODO: 1. Seperate the endpoints. There are too many in this file now.
+router.include_router(images.router)
 
 # * Optional todos:
-# TODO: 2. Learn jinja2. 
-# TODO: 3. Add a new endpoint for a readme.md file.
+# TODO: 1. Learn jinja2. 
+# TODO: 2. Add a new endpoint for a readme.md file.
+# import markdown as md
 # from fastapi.templating import Jinja2Templates
+
+
 # templates = Jinja2Templates(directory="docs")
 # # /API
 # @router.get("/", tags=["readme"], response_class=HTMLResponse)
@@ -25,14 +27,3 @@ router.include_router(piodash.router)
 #         "text": html
 #     }
 #     return templates.TemplateResponse("page.html", {"request": request, "data": data})
-
-# IMAGES
-@router.get("/colors/{img_url}", tags=["colors"])
-async def get_colors(img_url: str) -> list:
-    colors = get_dominant_colors(img_url)
-    return colors
-    
-@router.get("/colors/", tags=["colors"])
-async def get_colors(url: str) -> list:
-    return get_dominant_colors(url)
-
