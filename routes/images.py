05c1b7c3a4dbd985c5570c2d2241fe1e8e260543
Erik Beem <erik.beem@hotmail.com>
2021-05-12 09:39:25 +0200

    Updated endpoints.

--- routes/images.py
+++ routes/images.py
@@ -1,18 +1,19 @@
-from fastapi import APIRouter
-import asyncio
+from fastapi import APIRouter, UploadFile, File
 
-from services.dcr import get_dominant_colors
-from services.fetch_data import fetch_data
-
+from services.dcr import get_dominant_colors, read_img
 
 router = APIRouter()
+
+@router.post("/colors/upload", tags=["colors"])
+async def root(file: UploadFile = File(...)):
+    return {"file": file}
 
 @router.get("/colors/{img_url}", tags=["colors"])
 async def get_colors(img_url: str) -> list:
     colors = get_dominant_colors(img_url)
     return colors
     
-@router.get("/colors/", tags=["colors"])
-async def get_colors(url: str) -> list:
-    return get_dominant_colors(url)
+@router.get("/colors", tags=["colors"])
+async def index():
+    return 'Work in progress.'
 
