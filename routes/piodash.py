587808bbe21cc1451ac644db2e1fe3015df6a61f
Erik Beem <erik.beem@hotmail.com>
2021-05-12 09:39:16 +0200

    Refactored docstrings. Added Errorhandlers.

--- routes/piodash.py
+++ routes/piodash.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from services.dcr import get_dominant_colors
 from services.fetch_data import fetch_data
@@ -14,12 +14,15 @@ url = "https://dashboard-pio.herokuapp.com/companies"
 # Sync fetch
 db = fetch_data(url)['data']['response']
 
+
 @router.get("/piodash-colors/{company_id}", tags=["piodash-colors"])
-async def get_colors_of_a_company(company_id: str) -> list: 
+async def get_colors_of_a_company(company_id: str) -> list:
     """
-    Input: a company_id from the database.
-    Output: returns a list of hexadecimal RGB color values
+    Returns color values of a single company logo.
     """
+    if company_id not in db:
+        raise HTTPException(status_code=400, detail="ID not found.")
+    
     for i in range(len(db)):
         if company_id in db[i]['id']:
             url = url_base + db[i]['logo']
@@ -28,16 +31,14 @@ async def get_colors_of_a_company(company_id: str) -> list:
         else:
             continue
 
+
 @router.get("/piodash-colors/", tags=["piodash-colors"])
-async def get_colors_of_a_company() -> dict: 
+async def get_colors_of_companies() -> dict:
     """
-    Returns a dict with key = company_name and,
-    value = [logo_colors] where logo_colors is a list 
-    containing three hexadecimal RGB color-values.
-    Output: 
+    Returns color values of all the company logos in the piodash database.
     """
     piodash_color_dict = {}
-    
+
     for i in range(len(db)):
         company_name = db[i]['name']
         company_logo_url = url_base + db[i]['logo']
@@ -45,20 +46,9 @@ async def get_colors_of_a_company() -> dict:
             company_colors = get_dominant_colors(company_logo_url)
         except:
             company_colors = f"Function 'get_dominant_colors' returned an error."
-        
+
         piodash_color_dict[f'{company_name}'] = company_colors
     return piodash_color_dict
-
-
-@router.get("/piodash/", tags=["piodash-colors"])
-async def get_piodash_db() -> dict:
-    """
-    Simply returns a database for now.
-
-    Returns:
-        dict: [the fetched-database]
-    """
-    return db
 
 
 @router.get("/piodash/ids/", tags=["piodash-colors"])
@@ -66,12 +56,9 @@ async def get_piodash_company_ids():
     """"
     Returns a dict with company names as keys 
     and id's as values.
-    
-    Returns:
-        dict: [key: company_name, value: company_id]
     """
     piodash_id_dict = {}
-    
+
     for i in range(len(db)):
         company_id = db[i]['id']
         company_name = db[i]['name']
